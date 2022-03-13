1171b1ea6a318a3390e81c57d7cf96da53fc99b0
gokalp-ozturk <ozturkgokalp000@gmail.com>
2022-03-13 15:18:32 +0300

    backend code of rivo

---
+++ private/layouts/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+	<div class="row">
+		<div class="col">
+		RIVADEV YAZILIM SİSTEMLERİ LTD. ŞTİ. | Tüm hakları saklıdır! &copy; <?php echo date("Y"); ?>
+		</div>
+		<div class="col-auto">
+			<copyright data-toggle="tooltip" data-placement="top" title="Yazılım: Necati Mert">
+				<a href="https://rivadev.net/" rel="external">
+					RIVADEV YAZILIM
+				</a>
+			</copyright>
+		</div>
+	</div>
+</footer>
